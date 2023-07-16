8668cc938e3f06112ffc4a9c06787dc176522c39
Michael Pomatto <mike@pomatto.com>
2023-07-16 10:57:00 -0400

    Updated

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import requests 
-requests.get('https://api.github.com')
+import tensorflow as tf
+
+# Check for GPU availability
+gpus = tf.config.list_physical_devices('GPU')
+if not gpus:
+    print("No GPU available. Running on CPU.")
+    tf.config.set_visible_devices([], 'GPU')
+else:
+    # Limit GPU memory growth
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    print("GPU available. Running on GPU.")
+
+# Rest of your code
